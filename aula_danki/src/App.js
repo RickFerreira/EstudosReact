ceb1e68ca27cff9be97a6bf9f8d4aaf63d6bca58
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-05-06 17:13:04 -0300

    iniciando

--- aula_danki/src/App.js
+++ aula_danki/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+//sempre importar o react assign
+//useState serve para mudar os estados
+import React, {useState} from 'react';
 import './App.css';
+import MyBody from './MyBody.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //o que vai ficar inicialmente
+  const [str, setStr] = useState('Olá mundo');
+  //a funçaõ do click com a variavel que vai mudar a de cima
+  function handleClick(){
+    setStr("Hello World!!")
+  }
+  
+  return ( 
+    //chamando o h2 com o valor mutavel e o botão com o evento
+    <div className = "App" >
+      <h2> {str} </h2> 
+      <button onClick={()=>handleClick()}>Clique aqui</button>
+      <MyBody conteudo="Rick"></MyBody>
+      <MyBody conteudo="Paty"></MyBody>
+      <MyBody conteudo="Henrique"></MyBody>
     </div>
+    //com isso voce consegue chamar uma classe que tem um conteudo e trabalhar com eles 
+
   );
 }
 
---
+++ aula_danki/src/MyBody.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyBody = (props) => {
+
+    return (
+        <h2>Olá {props.conteudo}!</h2>
+    );
+
+}
+
+export default MyBody;
